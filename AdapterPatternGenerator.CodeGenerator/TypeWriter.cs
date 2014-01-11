d04c90e474327889872d73049ca9ee54aef17bb0
Eva Lacy <eva@lacy.ie>
2014-01-11 19:37:07 +0000

    Added basic ability to export code

--- AdapterPatternGenerator.CodeGenerator/TypeWriter.cs
+++ AdapterPatternGenerator.CodeGenerator/TypeWriter.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.CodeDom;
+using System.CodeDom.Compiler;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,8 +11,20 @@ namespace AdapterPatternGenerator.CodeGenerator
 {
     public class TypeWriter:ITypeWriter
     {
+        private const string FileName = "file.cs";
         public void WriteTypes(CodeCompileUnit codeCompileUnit, string directoryName)
         {
+            GenerateCSharpCode(codeCompileUnit,Path.Combine(directoryName,FileName));
+        }
+        public void GenerateCSharpCode(CodeCompileUnit codeCompileUnit, string fileName)
+        {
+            var provider = CodeDomProvider.CreateProvider("CSharp");
+            var options = new CodeGeneratorOptions {BracingStyle = "C"};
+            using (var sourceWriter = new StreamWriter(fileName))
+            {
+                provider.GenerateCodeFromCompileUnit(
+                    codeCompileUnit, sourceWriter, options);
+            }
         }
     }
 }
